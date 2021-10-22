6310c896edc58ca3d9a03c8fca29c24850485240
Abiodun <abiodunhodonu@gmail.com>
2021-10-22 10:54:51 +0100

    completed hero content

--- src/components/Landingpage/Hero.js
+++ src/components/Landingpage/Hero.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import hero1 from '../../images/hero1.png'
+import hero2 from '../../images/hero2.png'
+import hero3 from '../../images/hero3.png'
 
 const Hero = () => {
     return(
@@ -8,10 +11,18 @@ const Hero = () => {
                 <p>Bakery</p>
                 <p>Restaurants</p>
             </div>
-            <div className="side">
-            <h1 className="hero-side">Eat <span className="p-green">more for less</span> and fight food waste</h1>
-            <p>Pick up and eat all you can from fresh food items that are close to their expiry dates at discounted prices from your favourite restaurants and grocery stores.</p>
+                <div className="side">
+                <h1 className="hero-side">Eat <span className="p-green">more for less</span> and fight food waste</h1>
+                <p>Pick up and eat all you can from fresh food items that are close to their expiry dates at discounted prices from your favourite restaurants and grocery stores.</p>
+                <div className="hero-image1">
+                    <img src={hero1} alt="food" />
+                    <img src={hero2} alt="food" />
+                </div>
+                <div className="hero-image2">
+                    <img src={hero3} alt="food" />
+                </div>
             </div> 
+          
         </div>
     )
 
