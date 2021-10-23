1aef05c9510df3d3963327c3e18dfdac323ab415
Abiodun <abiodunhodonu@gmail.com>
2021-10-23 15:43:08 +0100

    created another page and imported Navbar and Footer component

---
+++ src/pages/Groceries.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Landingpage/Navbar'
+import Footer from '../components/Landingpage/Footer'
+
+const Groceries = () => {
+    return (
+        
+        <div>
+        <Navbar />
+        <p>Groceries</p>
+        
+        
+        <Footer />
+        </div>
+    )
+}
+
+export default Groceries;
