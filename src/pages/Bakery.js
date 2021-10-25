94f65a4274de9189f08901c5a8d41c765a9a6ac6
Abiodun <abiodunhodonu@gmail.com>
2021-10-25 17:17:30 +0100

    created a page for bakery

---
+++ src/pages/Bakery.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Landingpage/Navbar'
+import Footer from '../components/Landingpage/Footer'
+
+const Bakery = () => {
+    return (
+        
+        <div>
+            <Navbar />
+            <p>Bakery</p>
+            <Footer />
+        </div>
+    )
+}
+
+export default Bakery;
