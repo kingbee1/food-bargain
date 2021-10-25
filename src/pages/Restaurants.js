fa2b14e57a7128ff2edc795876937d2ccea6e82e
Abiodun <abiodunhodonu@gmail.com>
2021-10-25 17:16:33 +0100

    created a page for Restaurants

---
+++ src/pages/Restaurants.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/Landingpage/Footer';
+import Navbar from '../components/Landingpage/Navbar';
+
+const Restaurants = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Restaurants</p>
+            <Footer />
+        </div>
+    )
+}
+
+export default Restaurants;
