910ffb9bfa14e87c7e600cf9760d1cb352e70778
Abiodun <abiodunhodonu@gmail.com>
2021-10-26 14:31:03 +0100

    completed the grocery page

--- src/pages/Groceries.js
+++ src/pages/Groceries.js
@@ -2,7 +2,16 @@ import React from 'react'
 import Navbar from '../components/Landingpage/Navbar'
 import Footer from '../components/Landingpage/Footer'
 import food3 from '../images/food3.png'
-import food4 from '../images/food4.png'
+import gr1 from '../images/gr1.png'
+import gr2 from '../images/gr2.png'
+import gr3 from '../images/gr3.png'
+import gr4 from '../images/gr4.png'
+import gr5 from '../images/gr5.png'
+import gr6 from '../images/oil.png'
+import gr7 from '../images/flour.png'
+import gr8 from '../images/minimie.png'
+import gr9 from '../images/wine.png'
+
 
 const Groceries = () => {
     return (
@@ -51,25 +60,50 @@ const Groceries = () => {
                      </div >
                         <div className="Groc-pic flex justify-between">
                         <div>
-                        <img src={food4} alt="food" />
+                        <img src={gr1} alt="food" />
                         <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
                         <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
                     </div>
                     
                
                     <div>
-                        <img src={food3} alt="food" />
+                        <img src={gr2} alt="food" />
                         <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
                         <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
                     </div>
 
                     <div>
-                        <img src={food4} alt="food" />
+                        <img src={gr3} alt="food" />
                         <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
                         <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
                     </div>
                     <div>
-                        <img src={food3} alt="food" />
+                        <img src={gr4} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    <div className = "gr5">
+                        <img src={gr5} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    <div>
+                        <img src={gr6} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    <div>
+                        <img src={gr7} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    <div>
+                        <img src={gr8} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    <div>
+                        <img src={gr9} alt="food" />
                         <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
                         <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
                     </div>
@@ -83,7 +117,30 @@ const Groceries = () => {
             <div>
             </div>
         </div>
-                <p className="groc-bottom">People are currently looking at these </p>
+        <div>               
+            <p className="groc-bottom">People are currently looking at these </p>
+            <div className="Groc-pic2 flex justify-between">
+            <div>
+                        <img src={gr1} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+                    
+               
+                    <div>
+                        <img src={gr2} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+
+                    <div>
+                        <img src={gr3} alt="food" />
+                        <div className="flex justify-between"><p>Stir Fry Noodles</p> <p>-16</p></div>
+                        <p>N 5,400 &emsp;<span className="span">NGN 2,500</span></p>
+                    </div>
+            </div>
+        </div>
+ 
         <Footer />
         </div>
     )
